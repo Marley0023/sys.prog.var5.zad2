800a6271b779ff5a990062dbbaeaa778177545e3
Honor <Honor@ENROXES-L>
2024-11-22 23:24:00 +0500

    Добавьте файлы проекта.

---
+++ sys.prog.var5.zad2.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+#include <stdio.h>
+#define f pow(y,x) + cbrt(fabs(x) + pow(fabs(y), exp(x)))
+#define w sqrt(fabs(20*x)) / (pow(x, 2) + pow(y, 3))
+int main()
+{
+	setlocale(0, "rus");
+	const double x = -0.85;
+	const double y = 1.25;
+	printf("b = %f\t\n", f);
+	printf("z = %f\t\n", w);
+}
+
